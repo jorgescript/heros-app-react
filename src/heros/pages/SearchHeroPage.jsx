9bed1c4b6294405e49d46965374d29c23093cbd4
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-07-16 18:22:05 -0500

    Optimizando el componente de busqueda de heroes

--- src/heros/pages/SearchHeroPage.jsx
+++ src/heros/pages/SearchHeroPage.jsx
@@ -2,21 +2,22 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from "query-string";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components/HeroCard";
+import { getHerosByName } from "../helpers/getHerosByName";
 
 export const SearchHeroPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { q = "" } = queryString.parse(location.search);
+  const heros = getHerosByName(q);
 
-  const { searchText, onInputChange, onResetForm } = useForm({
-    searchText: "",
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
   });
 
   const onSearchSubmit = (e) => {
     e.preventDefault();
     if (searchText.trim().length <= 2) return;
     navigate(`?q=${searchText.toLowerCase().trim()}`);
-    onResetForm();
   };
 
   return (
@@ -41,9 +42,15 @@ export const SearchHeroPage = () => {
         </div>
         <div className="col-7">
           <h4>Results</h4>
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">There's no results with {q}</div>
-          {/* <HeroCard /> */}
+          {q === "" && <div className="alert alert-primary">Search a hero</div>}
+          {!heros.length && (
+            <div className="alert alert-danger">
+              There's no results with {q}
+            </div>
+          )}
+          {heros.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
