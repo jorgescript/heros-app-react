96872a6ce1bc17e622e7a487b4999405c88e0535
Jorge Mosqueda <mosqueda.ing@icloud.com>
2022-07-16 14:50:35 -0500

    Funcionalidad pagina hero

--- src/heros/pages/HeroPage.jsx
+++ src/heros/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers/getHeroById";
 
 export const HeroPage = () => {
@@ -7,5 +7,37 @@ export const HeroPage = () => {
 
   if (!hero) return <Navigate to="/marvel" />;
 
-  return <div>HeroPage</div>;
+  const urlHero = `/assets/heroes/${id}.jpg`;
+  const navigate = useNavigate();
+  const onNavigateBack = () => {
+    const publisherNav = hero.publisher.split(" ")[0].toLowerCase();
+    navigate(`/${publisherNav}`);
+  };
+
+  return (
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={urlHero} alt={hero.superhero} className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b> {hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+        <button className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Regresar
+        </button>
+      </div>
+    </div>
+  );
 };
